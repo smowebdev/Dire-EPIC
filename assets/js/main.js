a7917bbefb80e66392364e5ad07ac56e6e83ea50
smowebdev <smowebdev@gmail.com>
2026-09-01 19:23:28 +0700

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,4 +12,19 @@ $(document).ready(function () {
   $(document).on("click", function () {
     $(".header-mobile-menu").removeClass("active");
   });
+  $(".faq-question").on("click", function () {
+    const $item = $(this).closest(".faq-item");
+    const $answer = $item.find(".faq-answer");
+
+    $(".faq-item")
+      .not($item)
+      .removeClass("active")
+      .find(".faq-answer")
+      .stop(true, true)
+      .slideUp(300);
+
+    $item.toggleClass("active");
+
+    $answer.stop(true, true).slideToggle(300);
+  });
 });
